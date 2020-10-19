307ee1463532dec00064a8bb634a363341920934
EUBIN <41097199+danial1021@users.noreply.github.com>
2020-10-20 03:09:10 +0900

    footer 수정 기능 && 정리

--- src/views/site/title.vue
+++ src/views/site/title.vue
@@ -1,12 +1,13 @@
+
 <template>
   <v-toolbar-title>
     {{ title }}
-    <v-btn icon @click="openDialog"><v-icon>mdi-grease-pencil</v-icon></v-btn>
+    <v-btn icon @click="openDialog"><v-icon>mdi-pencil</v-icon></v-btn>
     <v-dialog v-model="dialog" max-width="400">
       <v-card>
         <v-card-title>
           제목 수정
-          <v-spacer />
+          <v-spacer/>
           <v-btn icon @click="save"><v-icon>mdi-content-save</v-icon></v-btn>
           <v-btn icon @click="dialog=false"><v-icon>mdi-close</v-icon></v-btn>
         </v-card-title>
@@ -17,6 +18,7 @@
     </v-dialog>
   </v-toolbar-title>
 </template>
+
 <script>
 export default {
   props: ['title'],
