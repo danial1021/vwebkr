b4500a4481e303cfccc86d4d5ce36be10f779298
EUBIN <41097199+danial1021@users.noreply.github.com>
2020-10-22 02:08:49 +0900

    functions 사용자 추가
    기존의 요금 정책이 바뀌었음. (무료->유료)

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,27 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+var admin = require('firebase-admin')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+var serviceAccount = require('./key.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://vwebkr-6a0db.firebaseio.com'
+})
+
+const db = admin.database()
+
+exports.createUser = functions.auth.user().onCreate(async (user) => {
+  const { uid, email, displayName, photoURL } = user
+  const u = {
+    email,
+    displayName,
+    photoURL,
+    createdAt: new Date()
+  }
+  db.ref('users').child(uid).set(u)
+})
+
+exports.deleteUser = functions.auth.user().onDelete(async (user) => {
+  const { uid } = user
+  db.ref('users').child(uid).remove()
+})
